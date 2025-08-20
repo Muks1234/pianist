64c7c6b95911f83274dfc101213c6fd1ac6a7558
Muks1234 <mooksy3@gmail.com>
2025-08-20 02:15:32 +0200

    2nd commit of pianist project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { playNote, playScale } from "./piano";
+import "./App.css";
 
 function App() {
+  // Define all 88 piano keys (A0 to C8)
+  const notes = [
+    "A0", "A#0", "B0",
+    "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
+    "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
+    "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
+    "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
+    "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
+    "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6",
+    "C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7",
+    "C8"
+  ];
+
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>🎹 Virtual Pianist</h1>
-      <button onClick={() => playScale()}>Play Scale</button>
-      <div style={{ marginTop: "20px" }}>
-        {["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"].map((note) => (
+    <div className="piano-container">
+      <h1 className="piano-title">🎹 Virtual Pianist</h1>
+      <button className="play-scale-btn" onClick={() => playScale()}>Play Scale</button>
+      <div className="piano-keys">
+        {notes.map((note) => (
           <button
             key={note}
             onClick={() => playNote(note)}
-            style={{ margin: "5px", padding: "10px 20px" }}
+            className={`key ${note.includes("#") ? "black-key" : "white-key"}`}
           >
             {note}
           </button>
